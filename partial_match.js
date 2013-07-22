4d844116f37aceb14f0be92088a6ac0154960e88
Jeremy Herr <jeremy.herr@gmail.com>
2013-07-23 00:02:25 +0100

    Got very basic matchWords working

--- partial_match.js
+++ partial_match.js
@@ -1,7 +1,34 @@
-function matchWords (A, B) {
+(function () {
+	// compile the regexp once
+	var whiteSpace = RegExp("\\s+");
 
-}
+	/**
+	 * Compares 2 lines of text, returning the number of words in common, sort of.
+	 * Meant to be quick. It splits up the first string into words and looks for exact copies in the second string.
+	 *
+	 * @param {String} A a line of text
+	 * @param {String} B a line of text
+	 */
+	function matchWords (A, B) {
+		var wordsA = A.split(whiteSpace),
+			wordsB = B.split(whiteSpace),
+			i,
+			j,
+			numMatches = 0;
 
-if (typeof (exports) === 'object') {
-    exports.matchWords = matchWords;
-}
+		for (i = wordsA.length - 1; i >= 0; i--) {
+			for (j = wordsB.length - 1; j >= 0; j--) {
+				if (wordsA[i] === wordsB[j]) {
+					numMatches++;
+					break;
+				}
+			}
+		}
+
+		return numMatches;
+	}
+
+	if (typeof (exports) === 'object') {
+		exports.matchWords = matchWords;
+	}
+})();
--- spec/partial_match.spec.js
+++ spec/partial_match.spec.js
@@ -3,7 +3,8 @@ var partialMatch = require("../partial_match");
 describe("partialMatch function", function () {
 	it("returns 8 when sentences with 8 words are identical", function () {
 		var sentence = "Here is my first sentence, with some punctuation.";
-		expect(partialMatch.matchWords(sentence, sentence)).toBe(1);
+
+		expect(partialMatch.matchWords(sentence, sentence)).toBe(8);
 	});
 
 	it("returns 0 when sentences are share no words", function () {
@@ -13,11 +14,10 @@ describe("partialMatch function", function () {
 		expect(partialMatch.matchWords(sentence1, sentence2)).toBe(0);
 	});
 
-	it("returns a number between 0 and 1 when some words match", function () {
+	it("returns 5 when 5 words match", function () {
 		var sentence1 = "and only certain words match",
-			sentence2 = "only certain things match and words must be in correct order";
+			sentence2 = "only certain things match and words don't need to be in correct order";
 
-		// not sure what I want here. Maybe start by just counting number of matching words, regardless of order
-		expect(partialMatch.matchWords(sentence1, sentence2)).toBe(2);
+		expect(partialMatch.matchWords(sentence1, sentence2)).toBe(5);
 	});
 });
