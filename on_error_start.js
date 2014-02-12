4d242df78edb3d72c7dbff7e4dca8def4e6b6ce7
Jeremy Herr <jeremy.herr@gmail.com>
2014-02-12 11:13:10 +0000

    Chrome extension now opens web socket connection and sends error info
     - This opens a new socket on every single page load. I need to move this
       to a background or event page.

--- on_error_start.js
+++ on_error_start.js
@@ -1,10 +1,46 @@
 console.log("running extension - start");
 
+// Connect to server
+var socket = io.connect('http://localhost:3001');
+
+// Listen for unique session ID from server.
+// This will be used to match up this server with browser plugin
+socket.on("new session", function (data) {
+    console.log("session ID: ", data.sessionId);
+});
+
+// TODO: possibly use this when I get background/event page working, possibly that's better.
+function sendError (msg, url, lineNumber) {
+	// instead of doing the websocket connection inside the webpage, it should send a message to the extension, and the extension should send the message
+	// socket.emit("chunk", { time: (new Date).getTime(), chunk: msg + " at " + url + " : " + lineNumber });
+
+	chrome.runtime.sendMessage("icdabehbkpkdnlkcillbpmjpegfehbgl", "message from content script to extension", {}, function () {
+		console.log("callback");
+	});
+
+}
+
+window.addEventListener("message", receiveMessage, false);
+
+function receiveMessage(event)
+{
+	// We only accept messages from ourselves
+	if (event.source != window) {
+		return;
+	}
+
+	if (event.origin.match("http://localhost")) {
+		socket.emit("chunk", event.data);
+	}
+}
+
 var embeddedCodeStart = "(" + function () {
 
-	function submitError () {
+	function submitError (msg, url, lineNumber) {
 		// TODO: submit error to node server (already exists in node-tee)
-		console.log("submitError", arguments);
+		console.log("submitError", msg, "at", url, ":", lineNumber);
+
+		window.postMessage({ message: msg, url: url, line: lineNumber }, "*");
 	};
 
 	console.log('embedded start');
@@ -13,9 +49,7 @@ var embeddedCodeStart = "(" + function () {
 		submitError.apply(this, arguments);
 	};
 
-	window.onkeydown = function () {
-		console.log('ON KEY DOWN');
-	};
+	window.onerror_start = window.onerror;
 
 } + ")()";
 
