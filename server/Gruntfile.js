9cf42f0fe365739f6d1f783ef7411e83eb2aadd0
Jeremy Herr <jeremy.herr@gmail.com>
2014-09-02 10:48:27 +0100

    Added browserify to default grunt task

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -1,23 +1,31 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON("package.json"),
 		react: {
 			dynamic_mappings: {
 				files: [
 					{
 						expand: true,
-						cwd: 'public/js-src',
-						src: ['**/*.js'],
-						dest: 'public/js',
-						ext: '.js'
+						cwd: "public/js-src",
+						src: ["**/*.js"],
+						dest: "public/js",
+						ext: ".js"
 					}
 				]
 			}
 		},
+		browserify: {
+			admin_page: {
+				src: [ "./public/js/StatusPage.js" ],
+				dest: "./public/js/bundle.js"
+			}
+		}
+
 	});
 
-	grunt.loadNpmTasks('grunt-react');
+	grunt.loadNpmTasks("grunt-react");
+	grunt.loadNpmTasks("grunt-browserify");
 
-	grunt.registerTask('default', ['react']);
+	grunt.registerTask("default", ["react", "browserify"]);
 };
